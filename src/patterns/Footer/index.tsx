8d4b225da194959c20106ba3e2b1db8efab74faf
Juliano Costa <88951981+JlnCosta82@users.noreply.github.com>
2022-02-23 23:31:14 -0300

    Add files via upload

---
+++ src/patterns/Footer/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+   font-size: 5px;
+   text-align: center;
+   color: red;
+`;
+
+
+function Footer(){
+    return (
+   	    
+        <Container>
+            <small>Igreja Batista Nikkei - 2022</small>
+        </Container>
+
+   	);
+}
+
+
+export default Footer;
