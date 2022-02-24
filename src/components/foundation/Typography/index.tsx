deec4bf2bdf88d0951e13d17b3c5698b3cb3a069
Juliano Costa <88951981+JlnCosta82@users.noreply.github.com>
2022-02-23 23:23:48 -0300

    Add files via upload

---
+++ src/components/foundation/Typography/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Typography({ children }){
+    return (
+   	    <p>
+           { children}
+   	    </p>
+   	);
+}
+
+Typography.propTypes = {
+	children: PropTypes.node,
+}
+
+export default Typography;
+
+
